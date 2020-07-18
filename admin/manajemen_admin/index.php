73baee487e9a9795c8458a2c026fc19cf5ee6c73
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-18 13:57:21 +0800

    Display admin data

--- admin/manajemen_admin/index.php
+++ admin/manajemen_admin/index.php
@@ -1,8 +1,11 @@
 <?php
 include "../../utils.php";
 include_once "../../config.php";
+include_once "../../connection/connection.php";
 
 authenticate();
+
+$result = $connection->query("SELECT * FROM admin WHERE tipe_admin != 'super admin'");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,16 +32,37 @@ authenticate();
                         <th class="border px-4 py-2">Nama</th>
                         <th class="border px-4 py-2">Email</th>
                         <th class="border px-4 py-2">Telp</th>
+                        <th class="border px-4 py-2">Aktif</th>
+                        <th class="border px-4 py-2">Tipe</th>
+                        <th class="border px-4 py-2">Tgl Tambah</th>
+                        <th class="border px-4 py-2">Tgl Update</th>
                     </tr>
                 </thead>
                 <tbody>
+                    <?php while ($row = $result->fetch_row()) {
+                        $id_admin   = $row[0];
+                        $nama       = $row[1];
+                        $email      = $row[2];
+                        $no_telp    = $row[3];
+                        $aktif      = $row[5];
+                        $tipe_admin = $row[6];
+                        $created_at = $row[7];
+                        $updated_at = $row[7];
+                    ?>
                     <tr>
-                        <td class="border px-4 py-2">1</td>
-                        <td class="border px-4 py-2">Adam</td>
-                        <td class="border px-4 py-2">adam@gmail.com</td>
-                        <td class="border px-4 py-2">858</td>
+                        <td class="border px-4 py-2"><?php echo $id_admin; ?></td>
+                        <td class="border px-4 py-2"><?php echo $nama; ?></td>
+                        <td class="border px-4 py-2"><?php echo $email; ?></td>
+                        <td class="border px-4 py-2">
+                            <a class="text-blue-600 underline" href="telp:<?php echo $no_telp; ?>"><?php echo $no_telp; ?></a>
+                        </td>
+                        <td class="border px-4 py-2"><?php echo $aktif == 1 ? 'aktif' : 'tidak aktif'; ?></td>
+                        <td class="border px-4 py-2"><?php echo $tipe_admin; ?></td>
+                        <td class="border px-4 py-2"><?php echo $created_at; ?></td>
+                        <td class="border px-4 py-2"><?php echo $updated_at; ?></td>
                     </tr>
-                    <tr>
+                    <?php } ?>
+                    <!-- <tr>
                         <td class="border px-4 py-2">2</td>
                         <td class="border px-4 py-2">Adam</td>
                         <td class="border px-4 py-2">Adam</td>
@@ -49,7 +73,7 @@ authenticate();
                         <td class="border px-4 py-2">Chris</td>
                         <td class="border px-4 py-2">Chris</td>
                         <td class="border px-4 py-2">1,280</td>
-                    </tr>
+                    </tr> -->
                 </tbody>
             </table>
         </div>

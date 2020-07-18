57b67bc96b5aef8a6fba12702ab0745a34ea4bab
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-18 12:27:12 +0800

    Add header and some design

--- admin/index.php
+++ admin/index.php
@@ -12,19 +12,12 @@ if (!isset($_SESSION['logged_in'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link href="/asti/css/main.css" rel="stylesheet">
     <title>Dashboard - ASTI</title>
 </head>
 
 <body class="font-sans min-h-screen bg-gray-200">
-    <header class="flex bg-blue-500 text-white px-5 md:items-center">
-        <span class="brand text-2xl font-bold">ASTI</span>
-    </header>
-
-    <nav>
-        <ul>
-            <li><a href="#">Dashboard</a></li>
-        </ul>
-    </nav>
+    <?php require_once "../header.php"; ?>
 </body>
 
 </html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<header class="flex bg-blue-500 text-white pl-2 pr-5 md:items-center">
+    <h3 class="brand text-3xl font-bold">ASTI</h3>
+</header>
+
+<nav class="py-3 sm:h-full sidenav text-white fixed">
+    <ul>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Manajemen Admin</a></li>
+        <li><a href="#">Manajemen Instansi</a></li>
+        <li><a href="#">Manajemen Komponen</a></li>
+        <li><a href="#">Manajemen Pegawai</a></li>
+        <li><a href="#">Manajemen Peminjam</a></li>
+        <li><a href="/asti/logout.php">Logout</a></li>
+    </ul>
+</nav>
+
+<main class="main">
+    <h3 class="text-2xl font-bold p-2 page-header">Dashboard</h3>
+</main>

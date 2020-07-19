cb1f6f2af355facc0de991773969014d392139de
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-20 01:12:08 +0800

    Add delete function for admin

---
+++ admin/manajemen_admin/delete.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "../../connection/connection.php";
+require_once "../../utils.php";
+
+if (!isset($_GET["id_admin"]) && !is_numeric($_GET["id_admin"])) {
+    redirect($_SERVER['HTTP_REFERER']);
+}
+
+$id_admin = $_GET["id_admin"];
+$query = "DELETE FROM admin WHERE id_admin = $id_admin";
+$result = $connection->query($query);
+if ($result) {
+    redirect($_SERVER['HTTP_REFERER']);
+}

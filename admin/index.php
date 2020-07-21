2fb3065caa12aa53adeebcbd64d8bbea71603a4a
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-21 11:44:35 +0800

    Cleanup

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,4 @@
-<?php
+<?
 include "../utils.php";
 include "../config.php";
 
@@ -10,18 +10,18 @@ authenticate();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?php echo BASE_PATH; ?>/css/tailwind.min.css" rel="stylesheet">
-    <link href="<?php echo BASE_PATH; ?>/css/main.css" rel="stylesheet">
+    <link href="<? echo BASE_PATH; ?>/css/tailwind.min.css" rel="stylesheet">
+    <link href="<? echo BASE_PATH; ?>/css/main.css" rel="stylesheet">
     <title>Dashboard - ASTI</title>
 </head>
 
 <body class="font-sans min-h-screen bg-gray-200 overflow-hidden text-sm">
-    <?php require_once "../header.php"; ?>
+    <? require_once "../header.php"; ?>
 
     <main class="main lg:ml-64">
         <h3 class="text-2xl font-bold py-2 page-header">Dashboard</h3>
     </main>
-    <?php require_once "../scripts.php"; ?>
+    <? require_once "../scripts.php"; ?>
 </body>
 
 </html>

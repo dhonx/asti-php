e66e6c5567887e57e532de1fc0a55870da1c0a0d
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-22 20:44:03 +0800

    A11Y: Add role attribute on menu button

--- header.php
+++ header.php
@@ -3,11 +3,10 @@
 
 <header class="bg-blue-500 flex pl-2 pr-5 md:items-center text-white">
     <h3 class="brand font-bold ml-auto lg:ml-0 text-3xl"><a href="<?= BASE_PATH ?>">ASTI</a></h3>
-    <div class="cursor-pointer lg:hidden ml-auto md:flex self-center sm:flex xs:flex" id="mobile-menu">
+    <div class="cursor-pointer lg:hidden ml-auto md:flex self-center sm:flex xs:flex" id="mobile-menu" role="menu">
         <img src="<?= BASE_PATH ?>/img/open-menu.svg" alt="">
     </div>
 </header>
-
 <nav class="sidenav bg-blue-600 lg:w-64 absolute w-screen lg:block md:hidden py-3 sm:hidden text-white xs:hidden" id="nav-menu">
     <ul>
         <li><a href="<?= BASE_PATH ?>/admin">Dashboard</a></li>

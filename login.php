fea9e30fadb44a8433794b1cdfc4939b1ae0e77a
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-22 23:07:31 +0800

    Disable spellcheck on email field

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
 		<h2 class="text-2xl text-center my-2 font-bold">Login ASTI</h2>
 
 		<label class="mx-1" for="email">Email</label>
-		<input autocomplete="off" autofocus id="email" class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-lg" id="email" minlength="6" name="email" required type="email" value="donnisnoni.tid3@gmail.com">
+		<input autocomplete="off" autofocus id="email" class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-lg" id="email" minlength="6" name="email" required spellcheck="false" type="email" value="donnisnoni.tid3@gmail.com">
 
 		<label class="mx-1" for="password">Sandi</label>
 		<input class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="password" minlength="8" name="password" required type="password" value="donnisnoni@1234">

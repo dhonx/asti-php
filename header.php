27d77945c7215bae9b497e521ef318a32914e5f7
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-23 20:50:16 +0800

    Using font icon instead of image on menu button

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <header class="bg-blue-500 flex lg:hidden fixed w-screen lg:mb-10 pl-2 pr-5 items-center shadow text-white">
     <h3 class="brand font-bold ml-auto lg:ml-0 text-3xl"><a href="<?= BASE_PATH ?>">ASTI</a></h3>
     <div class="cursor-pointer lg:hidden ml-auto md:flex self-center sm:flex xs:flex" id="mobile-menu" role="menu">
-        <img src="<?= BASE_PATH ?>/img/open-menu.svg" alt="">
+        <span class="mdi mdi-big mdi-menu"></span>
     </div>
 </header>
 <nav class="bg-gray-200 hidden lg:flex p-2 sidenav" id="nav-menu" role="menu">

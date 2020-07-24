055aa34f8c952a15567de1e5b1bb32de6d233af3
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-24 23:34:11 +0800

    Add extra small media query breakpoint to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
     },
   },
   theme: {
+    screens: {
+      "xs": { "max": "640px" },
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {},
   },
   variants: {},

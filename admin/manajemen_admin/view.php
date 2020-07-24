53bab4587bf9a7b76303c49d5cc5f1a6fe187439
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-24 13:55:10 +0800

    Make primary button on view admin darker

--- admin/manajemen_admin/view.php
+++ admin/manajemen_admin/view.php
@@ -83,7 +83,7 @@ if (!isset($_GET["id_admin"]) && !is_numeric($_GET["id_admin"])) {
             </div>
             <div class="border border-b mt-2"></div>
             <div class="flex">
-                <a class="active-scale bg-blue-500 block py-2 px-3 mx-1 my-2 rounded-md text-white" href="update?id_admin=<?= $data["id_admin"] ?>" title="Ubah data ini">
+                <a class="active-scale bg-blue-900 block py-2 px-3 mx-1 my-2 rounded-md text-white" href="update?id_admin=<?= $data["id_admin"] ?>" title="Ubah data ini">
                     <span class="mdi align-middle mdi-pencil"></span>
                     Ubah
                 </a>

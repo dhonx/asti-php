97ff03ba545b3f14a23efe06850609568831a6c6
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-24 21:06:22 +0800

    Comment some uneeded code

--- temp/create_self.php
+++ temp/create_self.php
@@ -2,8 +2,8 @@
 
 include "../connection/connection.php";
 
-$nama =  "Don Alfonsus Nisnoni";
-$email = "donnisnoni@uyelindo.ac.id";
+// $nama =  "Don Alfonsus Nisnoni";
+// $email = "donnisnoni@uyelindo.ac.id";
 $password = password_hash('donnisnoni@1234', PASSWORD_BCRYPT);
 
 

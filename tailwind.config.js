98f33838c86559696d28829c2367cd1be506c156
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-25 00:42:52 +0800

    Add 'tailwindcss-dark-mode'

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,20 @@ module.exports = {
     enabled: true,
     content: ["./**/*.php"],
     options: {
-      whitelist: ["bg-blue-500"],
+      whitelist: ["bg-blue-500", "mode-dark"],
     },
   },
   theme: {
     screens: {
-      "xs": { "max": "640px" },
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      'dark': {'raw': '(prefers-color-scheme: dark)'},
+      xs: { max: "640px" },
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      dark: { raw: "(prefers-color-scheme: dark)" },
     },
     extend: {},
   },
   variants: {},
-  plugins: [
-    require('postcss-import'),
-  ],
+  plugins: [require("postcss-import"), require("tailwindcss-dark-mode")()],
 };

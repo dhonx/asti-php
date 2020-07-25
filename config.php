58f1176eb84cd5aeb15ddc790d0f4d1a33e146d7
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-26 01:52:36 +0800

    Fix email validation message

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define("BASE_PATH", "/asti");
 define("VALIDATION_MESSAGES", [
     "required"  => ":attribute harus diisi",
-    "email"     => ":email bukan email yang valid",
+    "email"     => ":value bukan email yang valid",
     "min"       => ":attribute minimal berisi :min karakter/digit",
     "max"       => ":attribute maximal berisi :max karakter/digit",
     "same"      => "Konfirmasi sandi harus sama dengan sandi",

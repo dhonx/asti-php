a298ec86cff24c6792519eaa2aa7feea199c5695
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-25 19:39:53 +0800

    Disable dark for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       md: "768px",
       lg: "1024px",
       xl: "1280px",
-      dark: { raw: "(prefers-color-scheme: dark)" },
+      // dark: { raw: "(prefers-color-scheme: dark)" },
     },
     extend: {},
   },

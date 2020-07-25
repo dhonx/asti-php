3a800d7e04dbc2391741d55b45424c109f98a3a0
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-25 15:19:20 +0800

    Comment uneeded code

--- scripts.php
+++ scripts.php
@@ -46,16 +46,16 @@
             }
         }
 
-        handle_sidenav()
+        handle_sidenav();
 
-        function checkDarkMode() {
-            return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-        }
+        // function checkDarkMode() {
+        //     return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        // }
 
-        if (checkDarkMode()) {
-            document.documentElement.classList.add('mode-dark');
-        } else {
-            document.documentElement.classList.remove('mode-dark');
-        }
+        // // if (checkDarkMode()) {
+        // //     document.documentElement.classList.add('mode-dark');
+        // // } else {
+        // //     document.documentElement.classList.remove('mode-dark');
+        // // }
     })();
 </script>

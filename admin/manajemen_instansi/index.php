bc293517a62625f44d9ed19a1160617fb0a126fc
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-07-31 21:26:30 +0800

    View search field title

--- admin/manajemen_instansi/index.php
+++ admin/manajemen_instansi/index.php
@@ -74,7 +74,7 @@ $result = $connection->query($query);
                 Tambah
             </a>
             <form class="flex ml-auto relative" method="get">
-                <input class="bg-gray-200 px-2 mx-2 rounded-md" placeholder="Cari..." name="keyword" title="Cari data admin" type="text" value="<?= $keyword ?>">
+                <input class="bg-gray-200 px-2 mx-2 rounded-md" placeholder="Cari..." name="keyword" title="Cari data instansi" type="text" value="<?= $keyword ?>">
                 <span class="absolute mdi mdi-magnify self-center" style="right:15px"></span>
             </form>
         </div>

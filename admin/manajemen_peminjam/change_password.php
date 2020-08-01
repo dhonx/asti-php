ee829e6e809d91c6faa511eb27c56ee2c76f5d93
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-08-02 04:00:08 +0800

    Cleanup

--- admin/manajemen_peminjam/change_password.php
+++ admin/manajemen_peminjam/change_password.php
@@ -62,7 +62,7 @@ if (isset($_POST["update_peminjam_password"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once "../../includes/css.php" ?>
-    <title>Ubah Sandi Pegawai - ASTI</title>
+    <title>Ubah Sandi Peminjam - ASTI</title>
 </head>
 
 <body class="flex font-sans min-h-screen overflow-hidden text-sm">
@@ -70,7 +70,7 @@ if (isset($_POST["update_peminjam_password"])) {
     <?php require_once "../../includes/header.php"; ?>
 
     <main class="flex flex-auto flex-col main">
-        <h3 class="text-2xl font-bold py-2 page-header">Ubah Sandi Pegawai</h3>
+        <h3 class="text-2xl font-bold py-2 page-header">Ubah Sandi Peminjam</h3>
 
         <form action="?id_peminjam=<?= $id_peminjam_to_update ?>" class="my-5 p-5 pb-2 rounded-md" method="post">
 

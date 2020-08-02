25e90978eb1bde7c704c333cdf166cbb09040963
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-08-02 23:37:11 +0800

    Refactor coding style & make more dynamic

--- admin/manajemen_admin/update.php
+++ admin/manajemen_admin/update.php
@@ -24,7 +24,6 @@ if ($r_get_admin && $r_get_admin->num_rows == 0) {
     redirect('./');
 }
 
-// It's a update mode
 if ($is_post) {
     $validator = new Validator(VALIDATION_MESSAGES);
 
@@ -57,6 +56,14 @@ if ($is_post) {
         }
     }
 }
+
+while ($row = $r_get_admin->fetch_assoc()) {
+    $data["nama"]       = $row["nama"];
+    $data["email"]      = $row["email"];
+    $data["no_telp"]    = $row["no_telp"];
+    $data["aktif"]      = $row["aktif"];
+    $data["tipe_admin"] = $row["tipe_admin"];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -65,7 +72,7 @@ if ($is_post) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once "../../includes/css.php" ?>
-    <title>Ubah Admin - ASTI</title>
+    <title>Ubah Admin <?= $data["nama"] ?> - ASTI</title>
 </head>
 
 <body class="flex font-sans min-h-screen overflow-hidden text-sm">
@@ -73,31 +80,16 @@ if ($is_post) {
     <?php require_once "../../includes/header.php"; ?>
 
     <main class="flex flex-auto flex-col main">
-        <h3 class="text-2xl font-bold py-2 page-header">Update Admin</h3>
+        <h3 class="text-2xl font-bold py-2 page-header">Ubah Admin <?= $data["nama"] ?></h3>
 
         <form action="?id_admin=<?= $id_admin_to_update ?>" class="my-5 p-5 pb-2 rounded-md" method="post">
-
             <?php if ($errors != null) { ?>
                 <div class="bg-red-400 p-2 mb-2 rounded-md text-white">
-                    <?php
-                    foreach ($errors as $error) {
-                        echo "<div>" .  $error . "</div>";
-                    }
-                    ?>
+                    <?php foreach ($errors as $error) { ?>
+                        <div><?= $error ?></div>
+                    <?php } ?>
                 </div>
             <?php } ?>
-
-            <?php
-            if (!$is_post) {
-                while ($row = $r_get_admin->fetch_assoc()) {
-                    $data["nama"]       = $row["nama"];
-                    $data["email"]      = $row["email"];
-                    $data["no_telp"]    = $row["no_telp"];
-                    $data["aktif"]      = $row["aktif"];
-                    $data["tipe_admin"] = $row["tipe_admin"];
-                }
-            }
-            ?>
 
             <label class="block" for="nama">Nama <span class="text-red-500" title="Harus diisi">*</span></label>
             <input autofocus class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="nama" minlength="5" name="nama" required spellcheck="false" type="text" value="<?= $errors ? get_prev_field('nama') : $data['nama'] ?>">
@@ -111,10 +103,11 @@ if ($is_post) {
             <a class="block my-2" href="change_password?id_admin=<?= $id_admin_to_update ?>">Ganti sandi</a>
 
             <span class="block">Status</span>
-            <?php
-            if ($errors) $status_checked = get_prev_field("status") == "on";
-            else $status_checked = $data["aktif"] == 1;
-            ?>
+            <?php if ($errors) {
+                $status_checked = get_prev_field("status") == "on";
+            } else {
+                $status_checked = $data["aktif"] == 1;
+            } ?>
             <input class="bg-gray-200 inline-block ml-2 px-3 py-2" <?= $status_checked ? "checked" : "" ?> id="status" name="status" type="checkbox">
             <label class="cursor-pointer inline-block w-11/12" for="status">Aktif</label>
 

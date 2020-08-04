61109059f8ef63ee73f62e48b995dd9300a2a2a0
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-08-04 11:39:45 +0800

    Add `create` page for manajemen barang

---
+++ admin/manajemen_barang/create.php
@@ -0,0 +1,155 @@
+<?php
+require_once "../../utils.php";
+require_once "../../connection/connection.php";
+require_once "../../config.php";
+require_once "../../vendor/autoload.php";
+
+use Rakit\Validation\Validator;
+
+authenticate(["super_admin", "admin"]);
+
+$errors = [];
+
+if (isset($_POST["create_barang"])) {
+    $validator = new Validator(VALIDATION_MESSAGES);
+    $validation = $validator->make($_POST, [
+        "kode_inventaris" => "required",
+        "id_komponen"     => "required|numeric",
+        "jumlah"          => "required|numeric",
+        "harga_beli"      => "required|numeric",
+        "kondisi"         => ["required", $validator("in", ["baik", "rusak ringan", "rusak berat"])]
+    ]);
+    $validation->validate();
+
+    if ($validation->fails()) {
+        $errors = $validation->errors()->firstOfAll();
+    } else {
+        $kode_inventaris = $connection->real_escape_string(clean($_POST["kode_inventaris"]));
+        $id_komponen = $_POST["id_komponen"];
+        $jumlah = $_POST["jumlah"];
+        $harga_beli = $_POST["harga_beli"];
+        $kondisi = $connection->real_escape_string($_POST["kondisi"]);
+        $status = isset($_POST["status"]) ? 1 : 0;
+        $keterangan = $connection->real_escape_string(clean($_POST["keterangan"]));
+
+        $admin_email = $_SESSION["email"];
+        $q_get_id_admin = "SELECT `id_admin` FROM `admin` WHERE `email` = '$admin_email'";
+        $r_get_id_admin = $connection->query($q_get_id_admin);
+        $first_row = $r_get_id_admin->fetch_assoc();
+        $id_admin = $first_row["id_admin"];
+
+        $q_insert = "INSERT INTO `barang`
+                        (`kode_inventaris`, `id_komponen`, `jumlah`, `harga_beli`, `kondisi`, `aktif`, `keterangan`, `id_admin`) 
+                    VALUES
+                        ('$kode_inventaris', $id_komponen, $jumlah, $harga_beli, '$kondisi', '$status', '$keterangan', $id_admin)";
+
+        if ($connection->query($q_insert)) {
+            redirect("./");
+        }
+    }
+}
+
+$q_get_komponen = "SELECT `id_komponen`, `nama` FROM `komponen`";
+$r_get_komponen = $connection->query($q_get_komponen);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "../../includes/css.php" ?>
+    <title>Tambah Barang - ASTI</title>
+</head>
+
+<body class="flex font-sans min-h-screen overflow-hidden text-sm">
+    <?php require_once "../../includes/loading.php" ?>
+    <?php require_once "../../includes/header.php" ?>
+
+    <main class="flex flex-auto flex-col main">
+        <h3 class="text-2xl font-bold py-2 page-header">Tambah Barang</h3>
+
+        <form class="my-5 p-5 pb-2 rounded-md" method="post">
+            <?php if ($errors) { ?>
+                <div class="bg-red-400 p-2 mb-2 text-white">
+                    <?php foreach ($errors as $error) { ?>
+                        <div><?= $error ?></div>
+                    <?php } ?>
+                </div>
+            <?php } ?>
+
+            <label class="block" for="kode_inventaris">Kode Inventaris <span class="text-red-500" title="Harus diisi">*</span></label>
+            <input autofocus class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="kode_inventaris" name="kode_inventaris" required spellcheck="false" type="text" value="<?= $errors ? get_prev_field("kode_inventaris") : "" ?>">
+
+            <label class="block" for="id_komponen">Barang <span class="text-red-500" title="Harus diisi">*</span></label>
+            <select class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="id_komponen" name="id_komponen">
+                <?php $prev_value = $errors ? get_prev_field("id_komponen") : "" ?>
+                <?php while ($row = $r_get_komponen->fetch_assoc()) {
+                    $v_id_komponen = $row["id_komponen"];
+                    $v_nama_komponen = $row["nama"];
+                ?>
+                    <option <?= $prev_value == $v_id_komponen ? "selected" : "" ?> value="<?= $v_id_komponen ?>">
+                        <?= $v_nama_komponen ?>
+                    </option>
+                <?php } ?>
+            </select>
+
+            <label class="block" for="harga_beli">Harga Beli <span class="text-red-500" title="Harus diisi">*</span></label>
+            <input class="bg-gray-200 px-3 py-2 mb-2 rounded-md w-full" id="harga_beli" min="0.00" max="100.000.0000" name="harga_beli" required spellcheck="false" step="0.01" type="number" value="<?= $errors ? get_prev_field("harga_beli") : "" ?>">
+
+            <label class="block" for="jumlah">Jumlah <span class="text-red-500" title="Harus diisi">*</span></label>
+            <input class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="jumlah" name="jumlah" required type="number" value="<?= $errors ? get_prev_field("jumlah") : "" ?>">
+
+            <div>Total</div>
+            <output class="bg-gray-200 block px-3 py-2 mb-2 rounded-md w-full" for="harga_beli jumlah" id="total" name="total">0</output>
+
+            <label class="block" for="kondisi">Kondisi <span class="text-red-500" title="Harus diisi">*</span></label>
+            <select class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="kondisi" name="kondisi">
+                <option value="baik">Baik</option>
+                <option value="rusak_ringan">Rusak Ringan</option>
+                <option value="rusak_berat">Rusak Berat</option>
+            </select>
+
+            <span class="block">Status</span>
+            <?php $status = $errors ? get_prev_field("status") : "" ?>
+            <input class="bg-gray-200 inline-block ml-2 px-3 py-2" <?= $status == "" ? "checked" : "" ?> id="status" name="status" type="checkbox">
+            <label class="cursor-pointer inline-block w-11/12" for="status">Aktif</label>
+
+            <label class="block" for="keterangan">Keterangan</label>
+            <textarea class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="keterangan" name="keterangan" style="min-height: 200px;"><?= $errors ? get_prev_field("keterangan") : "" ?></textarea>
+
+            <div class="border-b border-solid my-2 w-full"></div>
+
+            <div class="flex justify-end">
+                <button class="active-scale bg-gray-300 font-bold block px-3 py-2 mx-1 my-2 rounded-md" title="Reset formulir" type="reset">
+                    <span class="mdi align-middle mdi-notification-clear-all"></span>
+                    Reset
+                </button>
+                <button class="active-scale bg-blue-900 font-bold block px-3 py-2 mx-1 my-2 text-white rounded-md" name="create_barang" title="Tambah Barang" type="submit">
+                    <span class="mdi align-middle mdi-plus"></span>
+                    Tambah
+                </button>
+            </div>
+        </form>
+    </main>
+    <?php require_once "../../includes/scripts.php" ?>
+    <script>
+        (function() {
+            var fieldHargaBeli = document.body.querySelector("#harga_beli"),
+                fieldJumlah = document.body.querySelector("#jumlah"),
+                outputTotal = document.body.querySelector("#total");
+
+            fieldHargaBeli.addEventListener("input", countTotal);
+            fieldJumlah.addEventListener("input", countTotal);
+
+            function countTotal() {
+                var hargaBeli = parseInt(fieldHargaBeli.value),
+                    jumlah = parseInt(fieldJumlah.value);
+                var total = hargaBeli * jumlah
+                outputTotal.textContent = isNaN(total) ? '0' : total;
+            }
+        })()
+    </script>
+</body>
+
+</html>

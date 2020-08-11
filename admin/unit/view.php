5e44023e8c6e178a3976d096d10d1cb3345b5fd8
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-08-11 13:00:36 +0800

    Add view detail page for manajemen unit

---
+++ admin/unit/view.php
@@ -0,0 +1,74 @@
+<?php
+require_once "../../utils.php";
+require_once "../../connection/connection.php";
+require_once "../../config.php";
+require_once "../../vendor/autoload.php";
+
+authenticate(["super_admin", "admin"]);
+
+if (!isset($_GET["id_unit"]) && !is_numeric($_GET["id_unit"])) {
+    redirect("./");
+}
+
+$id_unit = $_GET["id_unit"];
+$q_get_unit = "SELECT * FROM `unit` WHERE `id_unit` = $id_unit";
+$r_get_unit = $connection->query($q_get_unit);
+if ($r_get_unit && $r_get_unit->num_rows == 0) {
+    redirect('./');
+}
+
+// DEBUG
+// else {
+//     print_r($connection->error_list);
+//     die();
+// }
+
+while ($row = $r_get_unit->fetch_assoc()) {
+    $data["id_unit"]    = $row["id_unit"];
+    $data["nama"]       = $row["nama"];
+    $data["keterangan"] = $row["keterangan"];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "../../includes/css.php" ?>
+    <title>Detail Unit <?= $data["nama"] ?> - ASTI</title>
+</head>
+
+<body class="flex font-sans min-h-screen overflow-hidden text-sm">
+    <?php require_once "../../includes/loading.php" ?>
+    <?php require_once "../../includes/header.php" ?>
+
+    <main class="flex flex-auto flex-col main">
+        <h3 class="text-2xl font-bold py-2 page-header">Detail Unit <?= $data["nama"] ?></h3>
+
+        <div class="bg-white my-2 p-2 rounded-md">
+            <div>
+                <span class="font-bold">Nama:</span>
+                <span><?= $data["nama"] ?></span>
+            </div>
+            <div class="block mt-2">
+                <span class="block font-bold">Keterangan:</span>
+                <textarea class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" style="min-height: 200px;" readonly><?= $data["keterangan"] ?></textarea>
+            </div>
+            <div class="border border-b mt-2"></div>
+            <div class="flex">
+                <a class="active-scale bg-blue-900 block py-2 px-3 mx-1 my-2 rounded-md text-white" href="update?id_unit=<?= $data["id_unit"] ?>" title="Ubah data ini">
+                    <span class="mdi align-middle mdi-pencil"></span>
+                    Ubah
+                </a>
+                <a data-nama="<?= $data["nama"] ?>" class="active-scale bg-red-500 delete-link block py-2 px-3 mx-1 my-2 rounded-md text-white" href="delete?id_unit=<?= $data["id_unit"] ?>" title="Hapus data ini">
+                    <span class="mdi align-middle mdi-trash-can"></span>
+                    Hapus
+                </a>
+            </div>
+        </div>
+    </main>
+    <?php require_once "../../includes/scripts.php" ?>
+</body>
+
+</html>

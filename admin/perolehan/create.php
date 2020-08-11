370a77ce8d95db85ce72b6f4d02b7e6ed50fd20c
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-08-12 01:07:55 +0800

    Add main page & create page for manajemen perolehan

---
+++ admin/perolehan/create.php
@@ -0,0 +1,184 @@
+<?php
+require_once "../../utils.php";
+require_once "../../connection/connection.php";
+require_once "../../config.php";
+require_once "../../vendor/autoload.php";
+
+use Rakit\Validation\Validator;
+
+authenticate(["super_admin", "admin"]);
+
+$errors = [];
+$valid_status_values = ["pembelian", "bantuan", "penyesuaian stok"];
+
+if (isset($_POST["create_perolehan"])) {
+    $validator = new Validator(VALIDATION_MESSAGES);
+    $validation = $validator->make($_POST, [
+        "id_pemasok"    => "required|numeric",
+        "id_komponen"   => "required|numeric",
+        "tanggal"       => "required|date:Y-m-d",
+        "status"        => ["required", $validator("in", $valid_status_values)],
+        "harga_beli"    => "required|numeric",
+        "jumlah"        => "required|numeric"
+    ]);
+    $validation->validate();
+
+    if ($validation->fails()) {
+        $errors = $validation->errors()->firstOfAll();
+    } else {
+        $id_pemasok  = $_POST["id_pemasok"];
+        $id_komponen = $_POST["id_komponen"];
+
+        $q_check_pemasok = "SELECT `id_pemasok` FROM `pemasok` WHERE `id_pemasok` = $id_pemasok";
+        $r_check_pemasok = $connection->query($q_check_pemasok);
+
+        $q_check_komponen = "SELECT `id_komponen` FROM `komponen` WHERE `id_komponen` = $id_komponen";
+        $r_check_komponen = $connection->query($q_check_komponen);
+
+        if ($r_check_pemasok && $r_check_pemasok->num_rows == 0) {
+            array_push($errors, "Pemasok tidak valid");
+        } else if ($q_check_komponen && $r_check_komponen->num_rows == 0) {
+            array_push($errors, "Komponen tidak valid");
+        } else {
+            $tanggal    = convert_date($_POST["tanggal"]);
+            $status     = $_POST["status"];
+            $harga_beli = $_POST["harga_beli"];
+            $jumlah     = $_POST["jumlah"];
+            $keterangan = $connection->real_escape_string(clean($_POST["keterangan"]));
+
+            $q_insert_perolehan = "INSERT INTO `perolehan`
+                                    (`id_pemasok`, `tanggal`, `status`, `keterangan`)
+                                    VALUES
+                                    ($id_pemasok, '$tanggal', '$status', '$keterangan')";
+            $r_insert_perolehan = $connection->query($q_insert_perolehan);
+
+            if ($r_insert_perolehan) {
+                $last_insert_id = $connection->insert_id;
+                $q_insert_detail_perolehan = "INSERT INTO `detail_perolehan`
+                                                (`id_perolehan`, `id_komponen`, `harga_beli`, `jumlah`)
+                                              VALUES
+                                                ($last_insert_id, $id_komponen, $harga_beli, $jumlah)";
+                $r_insert_perolehan = $connection->query($q_insert_detail_perolehan);
+
+                if ($r_insert_perolehan) {
+                    redirect("./");
+                }
+
+                // Insert detail_perolehan failed, uncomment this for debugging purpose only
+                // else {
+                //     print_r($connection->error_list);
+                // }
+            }
+
+            // Insert perolehan failed, uncomment this for debugging purpose only
+            // else {
+            //     print_r($connection->error_list);
+            // }
+        }
+    }
+}
+
+$q_get_komponen = "SELECT `id_komponen`, `nama` FROM `komponen`";
+$r_get_komponen = $connection->query($q_get_komponen);
+
+$q_get_pemasok = "SELECT `id_pemasok`, `nama` FROM `pemasok`";
+$r_get_pemasok = $connection->query($q_get_pemasok);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "../../includes/css.php" ?>
+    <title>Tambah Perolehan - ASTI</title>
+</head>
+
+<body class="flex font-sans min-h-screen overflow-hidden text-sm">
+    <?php require_once "../../includes/loading.php" ?>
+    <?php require_once "../../includes/header.php" ?>
+
+    <main class="flex flex-auto flex-col main">
+        <h3 class="text-2xl font-bold py-2 page-header">Tambah Perolehan</h3>
+
+        <form class="my-5 p-5 pb-2 rounded-md" method="post">
+            <?php if ($errors) { ?>
+                <div class="bg-red-400 p-2 mb-2 text-white">
+                    <?php foreach ($errors as $error) { ?>
+                        <div><?= $error ?></div>
+                    <?php } ?>
+                </div>
+            <?php } ?>
+
+            <label class="block" for="id_komponen">Komponen <span class="text-red-500" title="Harus dipilih">*</span></label>
+            <select autofocus class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="id_komponen" name="id_komponen">
+                <?php $prev_value = $errors ? get_prev_field("id_komponen") : "" ?>
+                <?php while ($row = $r_get_komponen->fetch_assoc()) {
+                    $v_id_komponen = $row["id_komponen"];
+                    $v_nama_komponen = $row["nama"];
+                ?>
+                    <option <?= $prev_value == $v_id_komponen ? "selected" : "" ?> value="<?= $v_id_komponen ?>">
+                        <?= $v_nama_komponen ?>
+                    </option>
+                <?php } ?>
+            </select>
+
+            <div class="mt-1 mb-2">
+                <a href="../manajemen_komponen/create">Tambahkan komponen baru jika tidak ada</a>
+            </div>
+
+            <label class="block" for="id_pemasok">Pemasok <span class="text-red-500" title="Harus dipilih">*</span></label>
+            <select class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="id_pemasok" name="id_pemasok">
+                <?php $prev_value = $errors ? get_prev_field("id_pemasok") : "" ?>
+                <?php while ($row = $r_get_pemasok->fetch_assoc()) {
+                    $v_id_pemasok = $row["id_pemasok"];
+                    $v_nama_pemasok = $row["nama"];
+                ?>
+                    <option <?= $prev_value == $v_id_pemasok ? "selected" : "" ?> value="<?= $v_id_pemasok ?>">
+                        <?= $v_nama_pemasok ?>
+                    </option>
+                <?php } ?>
+            </select>
+
+            <div class="mt-1 mb-2">
+                <a href="../pemasok/create">Tambahkan pemasok baru jika tidak ada</a>
+            </div>
+
+            <label class="block" for="harga_beli">Harga <span class="text-red-500" title="Harus diisi">*</span></label>
+            <input class="bg-gray-200 px-3 py-2 mb-2 rounded-md w-full" id="harga_beli" min="0.00" max="100.000.0000" name="harga_beli" required spellcheck="false" step="0.01" type="number" value="<?= $errors ? get_prev_field("harga_beli") : "" ?>">
+
+            <label class="block" for="jumlah">Jumlah <span class="text-red-500" title="Harus diisi">*</span></label>
+            <input class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="jumlah" name="jumlah" required type="number" value="<?= $errors ? get_prev_field("jumlah") : "" ?>">
+
+            <label class="block" for="tanggal">Tanggal Pesan <span class="text-red-500" title="Harus diisi">*</span></label>
+            <input class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="tanggal" name="tanggal" required type="date" value="<?= $errors ? get_prev_field("tanggal") : "10" ?>">
+
+            <label class="block" for="status">Status <span class="text-red-500" title="Harus dipilih">*</span></label>
+            <select class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="status" name="status">
+                <?php $prev_value = $errors ? get_prev_field("status") : "" ?>
+                <?php foreach ($valid_status_values as $status_value) { ?>
+                    <option <?= $prev_value == $status_value ? "selected" : "" ?> value="<?= $status_value ?>"><?= ucwords($status_value) ?></option>
+                <?php } ?>
+            </select>
+
+            <label class="block" for="keterangan">Keterangan</label>
+            <textarea class="bg-gray-200 w-full px-3 py-2 mb-2 rounded-md" id="keterangan" name="keterangan" style="min-height: 200px;"><?= $errors ? get_prev_field("keterangan") : "-" ?></textarea>
+
+            <div class="border-b border-solid my-2 w-full"></div>
+
+            <div class="flex justify-end">
+                <button class="active-scale bg-gray-300 font-bold block px-3 py-2 mx-1 my-2 rounded-md" title="Reset formulir" type="reset">
+                    <span class="mdi align-middle mdi-notification-clear-all"></span>
+                    Reset
+                </button>
+                <button class="active-scale bg-blue-900 font-bold block px-3 py-2 mx-1 my-2 text-white rounded-md" name="create_perolehan" title="Tambah Perolehan" type="submit">
+                    <span class="mdi align-middle mdi-plus"></span>
+                    Tambah
+                </button>
+            </div>
+        </form>
+    </main>
+    <?php require_once "../../includes/scripts.php" ?>
+</body>
+
+</html>
---
+++ admin/perolehan/index.php
@@ -0,0 +1,249 @@
+<?php
+require_once "../../utils.php";
+require_once "../../config.php";
+require_once "../../connection/connection.php";
+require_once "../../common/page.php";
+
+authenticate(["super_admin", "admin"]);
+
+$valid_columns  = ["id_pemasok", "nama_komponen", "nama_pemasok", "harga_beli", "jumlah", "total", "tanggal", "status"];
+$common_data = processs_common_input($_GET, $valid_columns);
+
+$sort_by        = $common_data["sort_by"];
+$asc            = $common_data["asc"];
+$keyword        = $connection->real_escape_string(clean($common_data["keyword"]));
+$ipp            = $common_data["ipp"];
+$page           = $common_data["page"];
+$is_search_mode = $common_data["is_search_mode"];
+
+$search_query = "SELECT
+                    `perolehan`.*,
+                    `pemasok`.`nama` AS `nama_pemasok`,
+                    `detail_perolehan`.`harga_beli`,
+                    `detail_perolehan`.`jumlah`,
+                    `komponen`.`nama` AS `nama_komponen`,
+                    SUM(`detail_perolehan`.`harga_beli` * `detail_perolehan`.`jumlah`) `total`
+                FROM
+                    `perolehan`
+                INNER JOIN `detail_perolehan`
+                    ON `perolehan`.`id_perolehan` = `detail_perolehan`.`id_perolehan`
+                INNER JOIN `pemasok`
+                    ON `perolehan`.`id_pemasok` = `pemasok`.`id_pemasok`
+                INNER JOIN `komponen`
+                    ON `detail_perolehan`.`id_komponen` = `komponen`.`id_komponen`
+                WHERE ";
+$search_query .= build_search_query($keyword, ["`komponen`.`nama`", "`pemasok`.`nama`"]);
+$search_query .= " GROUP BY `perolehan`.`id_perolehan`";
+
+if ($is_search_mode) {
+    $count_all_result  = $connection->query("SELECT * FROM ($search_query) AS `perolehan_` ORDER BY $sort_by $asc");
+} else {
+    $count_all_result  = $connection->query("SELECT * FROM `perolehan`");
+}
+
+$total_items = $count_all_result->num_rows;
+$page_count  = get_page_count($total_items, $ipp);
+
+if ($page > $page_count) {
+    $page = $page_count;
+}
+
+$offset_limit = get_offset_limit($page, $ipp);
+$offset       = $offset_limit["offset"];
+$limit        = $offset_limit["limit"];
+
+if ($is_search_mode) {
+    $search_query .= " LIMIT $limit OFFSET $offset";
+    $query = "SELECT * FROM ($search_query) AS `perolehan_` ORDER BY $sort_by $asc";
+} else {
+    $query = "SELECT
+                    `perolehan`.*,
+                    `pemasok`.`nama` AS `nama_pemasok`,
+                    `detail_perolehan`.`harga_beli`,
+                    `detail_perolehan`.`jumlah`,
+                    `komponen`.`nama` AS `nama_komponen`,
+                    SUM(`detail_perolehan`.`harga_beli` * `detail_perolehan`.`jumlah`) `total`
+                FROM
+                    `perolehan`
+                INNER JOIN `detail_perolehan`
+                    ON `perolehan`.`id_perolehan` = `detail_perolehan`.`id_perolehan`
+                INNER JOIN `pemasok`
+                    ON `perolehan`.`id_pemasok` = `pemasok`.`id_pemasok`
+                INNER JOIN `komponen`
+                    ON `detail_perolehan`.`id_komponen` = `komponen`.`id_komponen`
+                GROUP BY `perolehan`.`id_perolehan`
+                LIMIT $limit OFFSET $offset";
+    $query = "SELECT * FROM ($query) AS `perolehan_` ORDER BY $sort_by $asc";
+}
+
+$result = $connection->query($query);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "../../includes/css.php" ?>
+    <title><?= $is_search_mode ? "Hasil pencarian dari $keyword - " : "" ?> Manajemen Perolehan - ASTI</title>
+</head>
+
+<body class="flex font-sans min-h-screen overflow-hidden text-sm">
+    <?php require_once "../../includes/header.php" ?>
+    <?php require_once "../../includes/loading.php" ?>
+
+    <main class="flex flex-auto flex-col main">
+        <h3 class="font-bold page-header py-2 text-2xl">Manajemen Perolehan</h3>
+        <div class="flex my-4">
+            <a class="active-scale bg-blue-900 font-bold mr-2 px-3 py-2 rounded-md text-white" href="create" role="button" title="Tambah Perolehan">
+                <span class="mdi align-middle mdi-plus"></span>
+                Tambah
+            </a>
+            <form class="flex ml-auto relative" method="get">
+                <input class="bg-gray-200 px-2 mx-2 rounded-md" placeholder="Cari..." name="keyword" title="Cari data perolehan" type="text" value="<?= $keyword ?>">
+                <span class="absolute mdi mdi-magnify self-center" style="right:15px"></span>
+            </form>
+        </div>
+
+        <?php if ($total_items > 0) { ?>
+            <table class="w-full">
+                <thead>
+                    <tr class="bg-gray-200 font-bold">
+                        <?php $asc_toggle = $asc == "asc" ? "desc" : "asc"  ?>
+                        <th class="hidden lg:table-cell lg:text-left p-2">
+                            <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "nama_komponen", "asc" => $asc_toggle])) ?>
+                            <a class="block" href="?<?= $url_query ?>">Komponen</a>
+                        </th>
+                        <th class="hidden lg:table-cell lg:text-left p-2">
+                            <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "nama_pemasok", "asc" => $asc_toggle])) ?>
+                            <a class="block" href="?<?= $url_query ?>">Pemasok</a>
+                        </th>
+                        <th class="hidden lg:table-cell lg:text-left p-2">
+                            <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "harga_beli", "asc" => $asc_toggle])) ?>
+                            <a class="block" href="?<?= $url_query ?>">Harga</a>
+                        </th>
+                        <th class="hidden lg:table-cell p-2">
+                            <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "jumlah", "asc" => $asc_toggle])) ?>
+                            <a class="block" href="?<?= $url_query ?>">Jumlah</a>
+                        </th>
+                        <th class="hidden lg:table-cell lg:text-left p-2">
+                            <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "total", "asc" => $asc_toggle])) ?>
+                            <a class="block" href="?<?= $url_query ?>">Total</a>
+                        </th>
+                        <th class="hidden lg:table-cell lg:text-left p-2">
+                            <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "tanggal", "asc" => $asc_toggle])) ?>
+                            <a class="block" href="?<?= $url_query ?>">Tanggal</a>
+                        </th>
+                        <th class="hidden lg:table-cell lg:text-left p-2">
+                            <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "status", "asc" => $asc_toggle])) ?>
+                            <a class="block" href="?<?= $url_query ?>">Status</a>
+                        </th>
+                        <th class="hidden lg:table-cell lg:text-right p-2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = $result->fetch_assoc()) {
+                        $id_perolehan   = $row["id_perolehan"];
+                        $nama_komponen  = $row["nama_komponen"];
+                        $nama_pemasok   = $row["nama_pemasok"];
+                        $tanggal        = $row["tanggal"];
+                        $harga_beli     = $row["harga_beli"];
+                        $jumlah         = $row["jumlah"];
+                        $total          = $row["total"];
+                        $status         = $row["status"];
+                    ?>
+                        <tr class="bg-white flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap">
+                            <td class="w-full lg:w-auto p-1 lg:text-left text-center block lg:table-cell relative lg:static">
+                                <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
+                                    Komponen
+                                </span>
+                                <?= $nama_komponen ?>
+                            </td>
+                            <td class="w-full lg:w-auto p-1 lg:text-left text-center block lg:table-cell relative lg:static">
+                                <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
+                                    Pemasok
+                                </span>
+                                <?= $nama_pemasok ?>
+                            </td>
+                            <td class="w-full lg:w-auto p-1 lg:text-left text-center block lg:table-cell relative lg:static">
+                                <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
+                                    Harga
+                                </span>
+                                <?= number_format($harga_beli, "2") ?>
+                            </td>
+                            <td class="w-full lg:w-auto p-1 text-center block relative lg:static">
+                                <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
+                                    Jumlah
+                                </span>
+                                <?= $jumlah ?>
+                            </td>
+                            <td class="w-full lg:w-auto p-1 lg:text-left text-center block lg:table-cell relative lg:static">
+                                <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
+                                    total
+                                </span>
+                                <?= number_format($total, "2") ?>
+                            </td>
+                            <td class="w-full lg:w-auto p-1 lg:text-left text-center block lg:table-cell relative lg:static">
+                                <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
+                                    Tanggal
+                                </span>
+                                <?= date_format(date_create($tanggal), "j F Y") ?>
+                            </td>
+                            <td class="w-full lg:w-auto p-1 lg:text-left text-center block lg:table-cell relative lg:static">
+                                <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
+                                    Status
+                                </span>
+                                <?= ucwords($status) ?>
+                            </td>
+                            <td class="w-full lg:w-auto p-1 flex relative lg:static">
+                                <div class="ml-auto">
+                                    <a href="view?id_perolehan=<?= $id_perolehan ?>" class="text-blue-400 text-lg p-1 hover:text-blue-600" role="button" title="Lihat detail">
+                                        <span class="mdi mdi-eye"></span>
+                                    </a>
+                                    <a href="update?id_perolehan=<?= $id_perolehan ?>" class="text-blue-400 text-lg p-1 hover:text-blue-600" role="button" title="Ubah">
+                                        <span class="mdi mdi-pencil"></span>
+                                    </a>
+                                    <a data-nama="Perolehan <?= $nama_komponen ?> yang diperoleh pada tanggal <?= $tanggal ?>" href="delete?id_perolehan=<?= $id_perolehan ?>" class="delete-link cursor-pointer text-red-400 text-lg p-1 hover:text-red-600" role="button" title="Hapus">
+                                        <span class="mdi mdi-trash-can"></span>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+
+            <!-- -------------------------- SELECT ITEMS PER PAGE ------------------------- -->
+            <div class="flex justify-center lg:justify-end my-3">
+                <form class="flex" method="get">
+                    <label class="self-center px-2" for="ipp">Baris per halaman</label>
+                    <select class="p-2 rounded-sm bg-gray-200" id="ipp" name="ipp" onchange="submitItemsPerPage(this)">
+                        <option <?= $ipp == 5 ? "selected" : "" ?> value="5">5</option>
+                        <option <?= $ipp == 10 ? "selected" : "" ?> value="10">10</option>
+                        <option <?= $ipp == 15 ? "selected" : "" ?> value="15">15</option>
+                    </select>
+                </form>
+            </div>
+
+            <!-- ------------------------------- PAGINATION ------------------------------- -->
+            <div class="flex w-full justify-center items-center mt-auto my-3 pagination">
+                <?php foreach (range(1, $page_count) as $page_num) { ?>
+                    <?php if ($page_count == 1) continue; ?>
+                    <?php $url_query = http_build_query(array_merge($_GET, ["page" => $page_num])) ?>
+                    <a class="bg-gray-200 m-1 px-2 py-1 rounded-sm <?= $page_num == $page ? "active" : "" ?>" href="?<?= $url_query ?>" role="button">
+                        <?= $page_num ?>
+                    </a>
+                <?php } ?>
+            </div>
+        <?php } else { ?>
+            <?php if ($is_search_mode) { ?>
+                <div class="text-lg text-center m-auto">Tidak ada hasil dari kata pencarian '<?= $keyword ?>'</div>"
+            <?php } else { ?>
+                <div class="text-lg text-center m-auto">Belum ada data</div>"
+            <?php } ?>
+        <?php } ?>
+    </main>
+    <?php require_once "../../includes/scripts.php"; ?>
+</body>
+
+</html>

a07277b8257ab4123543648b5efdf1a2d9b9e9e2
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-08-14 23:42:14 +0800

    Dont show some columns

--- admin/barang/index.php
+++ admin/barang/index.php
@@ -22,9 +22,9 @@ $search_query = "SELECT
                     `detail_perolehan`.`harga_beli`,
                     `barang`.`aktif`,
                     `barang`.`kondisi`,
-                    `detail_perolehan`.`jumlah`,
-                    SUM(`detail_perolehan`.`harga_beli` * `detail_perolehan`.`jumlah`) `total`,
-                    `admin`.`nama` AS `nama_admin`,
+                    -- `detail_perolehan`.`jumlah`,
+                    -- SUM(`detail_perolehan`.`harga_beli` * `detail_perolehan`.`jumlah`) `total`,
+                    -- `admin`.`nama` AS `nama_admin`,
                     `komponen`.`nama` AS `nama_komponen`
                 FROM
                     `barang` 
@@ -51,20 +51,20 @@ if ($is_search_mode) {
             `detail_perolehan`.`harga_beli`,
             `barang`.`aktif`,
             `barang`.`kondisi`,
-            `detail_perolehan`.`jumlah`,
-            SUM(`detail_perolehan`.`harga_beli` * `detail_perolehan`.`jumlah`) `total`,
-            `admin`.`nama` AS `nama_admin`,
+            -- `detail_perolehan`.`jumlah`,
+            -- SUM(`detail_perolehan`.`harga_beli` * `detail_perolehan`.`jumlah`) `total`,
+            -- `admin`.`nama` AS `nama_admin`,
             `komponen`.`nama` AS `nama_komponen`
         FROM
             `barang`
         INNER JOIN
                     `admin` ON `barang`.`id_admin` = `admin`.`id_admin`
         INNER JOIN
-            `komponen` ON `barang`.`id_komponen` = `komponen`.`id_komponen`
-        INNER JOIN
             `perolehan` ON `barang`.`id_perolehan` = `perolehan`.`id_perolehan`
         INNER JOIN
             `detail_perolehan` ON `perolehan`.`id_perolehan` = `detail_perolehan`.`id_perolehan`
+        INNER JOIN
+            `komponen` ON `detail_perolehan`.`id_komponen` = `komponen`.`id_komponen`
          GROUP BY `barang`.`id_barang`"
     );
 }
@@ -90,20 +90,20 @@ if ($is_search_mode) {
                     `detail_perolehan`.`harga_beli`,
                     `barang`.`aktif`,
                     `barang`.`kondisi`,
-                    `detail_perolehan`.`jumlah`,
-                    SUM(`detail_perolehan`.`harga_beli` * `detail_perolehan`.`jumlah`) `total`,
-                    `admin`.`nama` AS `nama_admin`,
+                    -- `detail_perolehan`.`jumlah`,
+                    -- SUM(`detail_perolehan`.`harga_beli` * `detail_perolehan`.`jumlah`) `total`,
+                    -- `admin`.`nama` AS `nama_admin`,
                     `komponen`.`nama` AS `nama_komponen`
                 FROM
                     `barang`
                 INNER JOIN
                     `admin` ON `barang`.`id_admin` = `admin`.`id_admin`
                 INNER JOIN
-                    `komponen` ON `barang`.`id_komponen` = `komponen`.`id_komponen`
-                INNER JOIN
                     `perolehan` ON `barang`.`id_perolehan` = `perolehan`.`id_perolehan`
                 INNER JOIN
                     `detail_perolehan` ON `perolehan`.`id_perolehan` = `detail_perolehan`.`id_perolehan`
+                INNER JOIN
+                    `komponen` ON `detail_perolehan`.`id_komponen` = `komponen`.`id_komponen`
                 GROUP BY `barang`.`id_barang`
                 LIMIT $limit OFFSET $offset";
     $query = "SELECT * FROM ($query) AS `barang_` ORDER BY $sort_by $asc";
@@ -128,12 +128,12 @@ $result = $connection->query($query);
     <main class="flex flex-auto flex-col main">
         <h3 class="font-bold page-header py-2 text-2xl">Manajemen Barang</h3>
         <div class="flex my-4">
-            <a class="active-scale bg-blue-900 font-bold mr-2 px-3 py-2 rounded-md text-white" href="create" role="button" title="Tambah Barang">
+            <!-- <a class="active-scale bg-blue-900 font-bold mr-2 px-3 py-2 rounded-md text-white" href="create" role="button" title="Tambah Barang">
                 <span class="mdi align-middle mdi-plus"></span>
                 Tambah
-            </a>
+            </a> -->
             <form class="flex ml-auto relative" method="get">
-                <input class="bg-gray-200 px-2 mx-2 rounded-md" placeholder="Cari..." name="keyword" title="Cari data barang" type="text" value="<?= $keyword ?>">
+                <input class="bg-gray-200 mx-2 px-3 py-2 rounded-md" placeholder="Cari..." name="keyword" title="Cari data barang" type="text" value="<?= $keyword ?>">
                 <span class="absolute mdi mdi-magnify self-center" style="right:15px"></span>
             </form>
         </div>
@@ -159,13 +159,13 @@ $result = $connection->query($query);
                             <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "harga_beli", "asc" => $asc_toggle])) ?>
                             <a class="block" href="?<?= $url_query ?>">Harga</a>
                         </th>
-                        <th class="hidden lg:table-cell text-center p-2">
+                        <!-- <th class="hidden lg:table-cell text-center p-2">
                             <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "jumlah", "asc" => $asc_toggle])) ?>
                             <a class="block" href="?<?= $url_query ?>">Jumlah</a>
-                        </th>
-                        <th class="hidden lg:table-cell text-center lg:text-left p-2">
+                        </th> -->
+                        <!-- <th class="hidden lg:table-cell text-center lg:text-left p-2">
                             <a class="block" href="?<?= $url_query ?>">Total</a>
-                        </th>
+                        </th> -->
                         <th class="hidden lg:table-cell p-2">
                             <?php $url_query = http_build_query(array_merge($_GET, ["sort_by" => "aktif", "asc" => $asc_toggle])) ?>
                             <a class="block" href="?<?= $url_query ?>">Status</a>
@@ -177,13 +177,13 @@ $result = $connection->query($query);
                     <?php while ($row = $result->fetch_assoc()) {
                         $id_barang       = $row["id_barang"];
                         $kode_inventaris = $row["kode_inventaris"];
-                        $harga_beli      = $row["harga_beli"];
-                        $aktif           = $row["aktif"];
-                        $kondisi         = $row["kondisi"];
-                        $jumlah          = $row["jumlah"];
-                        $total           = $row["total"];
-                        $nama_admin      = $row["nama_admin"];
                         $nama_komponen   = $row["nama_komponen"];
+                        $kondisi         = $row["kondisi"];
+                        $aktif           = $row["aktif"];
+                        $harga_beli      = $row["harga_beli"];
+                        // $jumlah          = $row["jumlah"];
+                        // $total           = $row["total"];
+                        // $nama_admin      = $row["nama_admin"];
                     ?>
                         <tr class="bg-white flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap">
                             <td class="w-full lg:w-auto p-1 lg:text-left text-center block lg:table-cell relative lg:static">
@@ -210,18 +210,18 @@ $result = $connection->query($query);
                                 </span>
                                 <?= number_format($harga_beli, 2) ?>
                             </td>
-                            <td class="w-full lg:w-auto p-1 text-center block lg:table-cell relative lg:static">
+                            <!-- <td class="w-full lg:w-auto p-1 text-center block lg:table-cell relative lg:static">
                                 <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
                                     Jumlah
                                 </span>
                                 <?= $jumlah ?>
-                            </td>
-                            <td class="w-full lg:w-auto p-1 text-center block lg:text-left lg:table-cell relative lg:static">
+                            </td> -->
+                            <!-- <td class="w-full lg:w-auto p-1 text-center block lg:text-left lg:table-cell relative lg:static">
                                 <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
                                     Total
                                 </span>
                                 <?= number_format($total, 2) ?>
-                            </td>
+                            </td> -->
                             <td class="w-full lg:w-auto p-1 text-center block lg:table-cell relative lg:static">
                                 <span class="lg:hidden absolute text-center top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase h-full">
                                     Status
